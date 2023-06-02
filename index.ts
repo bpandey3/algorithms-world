a9f66b2e521387f398384874e1b910a2158d85c3
bpandey3 <bpandey3@hotmail.com>
2023-06-02 12:35:39 -0400

    1 file modified

--- index.ts
+++ index.ts
@@ -1,9 +1,20 @@
 //import * as JSP from './js-pract';
 
-import Main from './sort-algo';
+// import Main from './sort-algo';
 
-const main = new Main();
-main.executeCode();
+// const main = new Main();
+// main.executeCode();
 
 // import execute from './recursion';
 // execute();
+
+
+const arr = [5, 5, 5, 2, 2, 2, 2, 2, 9, 4];
+const counts = {};
+
+for (const num of arr) {
+  console.log('counts[num]   ',counts, num )
+  counts[num] = counts[num] ? counts[num] + 1 : 1;
+}
+
+console.log(counts);
